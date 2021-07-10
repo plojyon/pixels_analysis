1ebddf5b6d9477bb8fb2028c2f564c54718e55b3
Yon Ploj <yon.ploj@gmail.com>
2021-07-11 00:48:09 +0200

    data preparation script now works when called from anywhere

--- analyse.py
+++ analyse.py
@@ -1,11 +1,18 @@
 #!python3
 # for use in combination with peterbot
 # github.com/plojyon/peter
+import os
+import sys
 
-with open("/tmp/pixels_decrypted.json", "r") as file:
+input_filename = "/tmp/pixels_decrypted.json";
+output_filename = os.path.join(os.path.dirname(sys.argv[0]), "data.json");
+
+with open(input_filename, "r") as file:
 	pixels = file.read();
 
+# replace as requested in the README
 pixels = pixels.replace('\\', '\\\\');
 pixels = pixels.replace("'", "\\'");
-with open("data.json", "w") as file:
+
+with open(output_filename, "w") as file:
 	file.write("data='"+pixels+"'");
